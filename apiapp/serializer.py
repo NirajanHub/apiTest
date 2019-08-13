c94ba0409621b735de6dd54263da09c2963c06a2
Nirajan Chapagain <nirajanc19@gmail.com>
2019-08-13 17:04:23 +0545

    first push

---
+++ apiapp/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import newsFeed
+
+
+class NewsfeedSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = newsFeed
+        fields = ('firstname', 'lastname', 'image')
+
+        image_url = serializers.SerializerMethodField('get_image_url')
+        def get_image_url(self, obj):
+            return obj.image.url
