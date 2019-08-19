df5753840d612c60e4bbf1fa0c7bd16dac8775e9
Nirajan Chapagain <nirajanchapagain@Nirajans-MacBook-Pro.local>
2019-08-19 22:30:27 +0545

    idk

--- memesimages/models.py
+++ memesimages/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class memesImages(models.Model):
-
-    images=models.ImageField(upload_to='pics')
+    images = models.ImageField(upload_to='pics')
+    category = models.CharField(max_length=2)
 
     def _str_(self):
         return self.images
