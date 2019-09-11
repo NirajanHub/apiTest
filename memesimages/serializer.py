2e58bc99920c1a32e4e5bfa652bc4ac71f0adea0
Nirajan Chapagain <nirajanc19@gmail.com>
2019-09-11 11:00:12 +0545

    post method tria;

--- memesimages/serializer.py
+++ memesimages/serializer.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import memesImages
 
 class ImagesSerealizer(serializers.ModelSerializer):
-    class Meta:
-        model=memesImages
-        fields=('__all__')
+    # class Meta:
+    #     model=memesImages
+    #     fields=('__all__')
+    #
+    #
+    page=serializers.CharFiled(max_length=2)
+
+    def create(self, validated_data):
+        return memesImages.object.create(**validated_data)
--- memesimages/views.py
+++ memesimages/views.py
@@ -5,12 +5,17 @@ from rest_framework.response import Response
 from .models import memesImages
 from .serializer import ImagesSerealizer
 
+
 class ImageList(APIView):
 
-    def get(self,request):
-        memesImages1=memesImages.objects.all()
-        serealizer2=ImagesSerealizer(memesImages1,many=True)
+    def get(self, request):
+        memesImages1 = memesImages.objects.all()
+        serealizer2 = ImagesSerealizer(memesImages1, many=True)
         return Response(serealizer2.data)
 
-    def post(self):
-        pass
+    def post(self, request):
+        postRequest = request.data.get()
+        serealizer = ImagesSerealizer(data=postRequest)
+        if serealizer.is_valid(raise_exception=True):
+            article_saved = serealizer.save()
+        return Response(serealizer.data)
