02052cebc88e5a9aaaa0b05632e46cd70babb283
Nirajan Chapagain <nirajanchapagain@Nirajans-MacBook-Pro.local>
2019-09-23 22:25:31 +0545

    pagination done

--- memesimages/views.py
+++ memesimages/views.py
@@ -8,15 +8,18 @@ from django.core.paginator import Paginator
 
 class ImageList(APIView):
     count = 0
-    def get(self, request):
 
+    def get(self, request):
         memesImages1 = memesImages.objects.all()
         paginator = Paginator(memesImages1, 20)
-        self.count+=1
+        self.count += 1
         serealizer2 = ImagesSerealizer(paginator.page(self.count), many=True)
         return Response(serealizer2.data)
 
-
-    def post(self,request):
-
-        pass
+    def post(self, request):
+        images = memesImages.objects.all()
+        paginator = Paginator(images,10)
+        serealizer = ImagesSerealizer(paginator.page(request.data.get('page')), many=True)
+        # if (serealizer.is_valid(raise_exception=True)):
+        print(serealizer.data)
+        return Response(serealizer.data)
