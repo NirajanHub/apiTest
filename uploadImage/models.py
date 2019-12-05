77272e9a205a06b30e43096573306221eb154090
Nirajan Chapagain <nirajanc19@gmail.com>
2019-12-05 13:20:20 +0545

    code written for commit

---
+++ uploadImage/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Image(models.Model):
+    property_id=models.ForeignKey(
+        'properties.Address',
+        null=False,
+        default=1,
+        on_delete=models.CASCADE
+
+    )
+    image=models.ImageField(upload_to=directory_path)
---
+++ uploadImage/views.py
@@ -0,0 +1,39 @@
+from django.http import JsonResponse
+from django.http.multipartparser import MultiPartParser
+from rest_framework import status
+from rest_framework.parsers import FormParser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api_project.uploadImage.helpers import modify_input_for_multiple_files
+
+from .serializer import ImagesSerealizer
+
+class ImageList(APIView):
+    parser_classes = (MultiPartParser,FormParser)
+
+    # def get(self,request):
+    #     all_images=Image.objects.all()
+    #     serializer=ImagesSerealizer(all_images,many=True)
+    #     return JsonResponse(serializer.data,safe=False)
+    #
+
+    def post(self,request,*args,**kwargs):
+            property_id=request.data['property_id']
+
+            images=dict((request.data).lists())['images']
+            flag=1
+            arr=[]
+            for img_name in images:
+                modified_data = modify_input_for_multiple_files(property_id,
+                                                                img_name)
+                file_serializer = ImagesSerealizer(data=modified_data)
+                if file_serializer.is_valid():
+                    file_serializer.save()
+                    arr.append(file_serializer.data)
+                else:
+                    flag = 0
+
+            if flag == 1:
+                return Response(arr, status=status.HTTP_201_CREATED)
+            else:
+                return Response(arr, status=status.HTTP_400_BAD_REQUEST)
