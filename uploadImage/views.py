5858d6ff2ca161e453760212ac956a8fa5fdc570
nirajanchapagain <nirajanc19@gmail.com>
2019-12-18 22:52:59 +0545

    django

--- uploadImage/views.py
+++ uploadImage/views.py
@@ -2,32 +2,15 @@ from django.core.paginator import Paginator
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-<<<<<<< HEAD
 
-=======
 from .models import Images
 from .serializer import ImagesSerealizer
->>>>>>> 958c6f1fae6b20c7b762874e5af83e4d5dc4925a
 from uploadImage.helpers import modify_input_for_multiple_files
 
 from .serializer import ImagesSerealizer
 
 
 class Image(APIView):
-<<<<<<< HEAD
-    # parser_classes = (MultiPartParser,FormParser)
-
-    def get(self, request):
-        all_images = Images.objects.all()
-        serializer = ImagesSerealizer(all_images, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    def post(self, request, *args, **kwargs):
-        category = request.data['category']
-        images = dict((request.data).lists())['images']
-        flag = 1
-        arr = []
-=======
     count = 0
 
     # parser_classes = (MultiPartParser,FormParser)
@@ -39,17 +22,16 @@ class Image(APIView):
 
     def get(self, request):
         memesImages1 = Images.objects.all()
-        paginator =  Paginator(memesImages1, 3)
+        paginator = Paginator(memesImages1, 3)
         self.count += 1
         serealizer2 = ImagesSerealizer(paginator.page(self.count), many=True)
         return Response(serealizer2.data)
 
-    def post(self,request,*args,**kwargs):
-        category=request.data['category']
-        images=dict((request.data).lists())['images']
-        flag=1
-        arr=[]
->>>>>>> 958c6f1fae6b20c7b762874e5af83e4d5dc4925a
+    def post(self, request, *args, **kwargs):
+        category = request.data['category']
+        images = dict((request.data).lists())['images']
+        flag = 1
+        arr = []
         for img_name in images:
 
             modified_data = modify_input_for_multiple_files(category, img_name)
