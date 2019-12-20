1cd5e40fd1eca99e2e0273c4d3e711da3d9a3a31
nirajanchapagain <nirajanc19@gmail.com>
2019-12-20 22:29:49 +0545

    change

--- api_project/wsgi.py
+++ api_project/wsgi.py
@@ -16,7 +16,7 @@ import sys
 root_path = os.path.abspath(os.path.split(__file__)[0])
 sys.path.insert(0, os.path.join(root_path, 'api_project'))
 sys.path.insert(0, root_path)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project/settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
 
 application = get_wsgi_application()
 
