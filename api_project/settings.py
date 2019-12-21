c97cdd2f834edcb6938721f153f8c14d1d7ba4f1
nirajanchapagain <nirajanc19@gmail.com>
2019-12-21 09:40:06 +0545

    change

--- api_project/settings.py
+++ api_project/settings.py
@@ -28,8 +28,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'pics')
+MEDIA_URL = 'pics/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
 # Application definition
 
 INSTALLED_APPS = [
