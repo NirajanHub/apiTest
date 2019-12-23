ecc538fee5506977dc1d2d211283352b9d961904
Nirajan Chapagain <nirajanc19@gmail.com>
2019-12-23 14:52:35 +0545

    commited

--- uploadImage/serializer.py
+++ uploadImage/serializer.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from .models import Images
 
 class ImagesSerealizer(serializers.ModelSerializer):
-    category=serializers.CharField()
-    images=serializers.ImageField()
     class Meta:
         model=Images
         # fields=('__all__')
