f0503b85585a4b8b61010eceb60491b6789e6359
Nirajan Chapagain <nirajanc19@gmail.com>
2019-12-24 16:09:31 +0545

    commit

--- api_project/settings.py
+++ api_project/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'memesimages',
     'uploadImage',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
