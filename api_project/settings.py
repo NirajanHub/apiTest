c5f093a8a159f7684266fc2af0eb21766f50d9a0
nirajanchapagain <nirajanc19@gmail.com>
2020-05-27 13:43:57 +0545

    cloudinary storage

--- api_project/settings.py
+++ api_project/settings.py
@@ -129,7 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = ''
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, ''),
+)
 django_heroku.settings(locals())
